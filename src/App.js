4ebc9cd12c3ab03a7ba05691d6224ebab752b682
Makariuz <samuel.eduardo23@hotmail.com>
2022-08-18 13:01:09 +0100

    meat prices scrap / continente

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    const getNews = async () => {
+      try {
+        const res = await axios.get("http://localhost:5001/");
+        setNews(res.data);
+     
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  getNews();
+
+  }, []);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <h1>
+            Test App - Scrapper
+          </h1>
+          <div className='cards-wrapper'>
+          {news && news.map(n => {
+          return ( <div className='cards'>
+            <p>{n.title}</p>
+            <p>{n.price}</p>
+            </div>)
+          })}
+          </div>
     </div>
   );
 }
